aaeb5f7e83865e4290824704f78c3289ca03c8b1
Ryan Token <ryantoken13@gmail.com>
2023-02-22 08:41:16 -0700

    Upgrade SvelteKit and deps

--- src/lib/components/ResizableIcon.svelte
+++ src/lib/components/ResizableIcon.svelte
@@ -3,7 +3,5 @@
 </script>
 
 <a href={url} target="_blank" rel="noreferrer">
-	<p>
-		<img src={src} alt={altText} loading="lazy" decoding="async" class="p-2 rounded-3xl w-72" >
-	</p>
+	<img src={src} alt={altText} loading="lazy" decoding="async" class="p-2 rounded-3xl w-72" />
 </a>
--- src/routes/feed.rss/+server.js
+++ src/routes/feed.rss/+server.js
@@ -30,12 +30,12 @@ export const GET = async () => {
 
 const render = (posts) =>
 (`<?xml version="1.0" encoding="UTF-8" ?>
-<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:content="http://purl.org/rss/1.0/modules/content/">
+<rss version="2.0" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:content="http://purl.org/rss/1.0/modules/content/">
 <channel>
 <title>${siteTitle}</title>
 <description>${siteDescription}</description>
+<language>en</language>
 <link>${siteURL}</link>
-<atom:link href="${siteURL}/feed.rss.xml" rel="self" type="application/rss+xml"/>
 ${posts
 	.map(
 		(post) => `
