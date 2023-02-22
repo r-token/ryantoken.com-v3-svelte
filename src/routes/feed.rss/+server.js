b804a93ac15fe15725408afd6c8c7cc9c246d6b1
Ryan Token <ryantoken13@gmail.com>
2023-02-22 09:22:59 -0700

    Fix quotes

--- src/routes/feed.rss/+server.js
+++ src/routes/feed.rss/+server.js
@@ -26,7 +26,7 @@ export const GET = async () => {
 
 const render = (posts) =>
 (
-`<rss xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:content="http://purl.org/rss/1.0/modules/content/ version="2.0"">
+`<rss xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:content="http://purl.org/rss/1.0/modules/content/" version="2.0">
 <channel>
 <title>${siteTitle}</title>
 <description>${siteDescription}</description>
